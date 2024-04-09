f56f49aa9d79f495b61eec680698ffc80c2ef5f4
rayjayatunga <rayjayatunga@gmail.com>
2024-04-09 16:55:46 -0400

    working pdf parsing with LLM analysis

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 import streamlit as st
 import fitz  # PyMuPDF
+from io import StringIO
+import cohere
+
+if "text_extracted" not in st.session_state:
+    st.session_state.text_extracted = False
+if "pdf_body" not in st.session_state:
+    st.session_state.pdf_body = ''
+
+#######################################
+# FUNCTION DEFINITIONS
+#######################################    
+def extract_job_duties(pdf_body):
+    co = cohere.Client(st.secrets["COHERE_API_KEY"])
+    message = f"You are analysing a resume and extracting information about a candidate's job experience and duties from their resume pdf content below. Respond only in the form of a python list with each element containing a unique job duty. DO NOT SIMPLY LIST THE CANDIDATE'S SKILLS BUT MAKE SURE TO FIND UNIQUE RESPONSIBILITIES AND ACOMPLISHMENTS. PDF CONTENT: {pdf_body}"
+    response = co.chat(
+        message= message.format(pdf_body),
+        model= 'command-r'
+    )
+    return response.text
+
+
+#######################################
+# MAIN STREAMLIT APP
+#######################################    
 
 # Enable PDF upload
 uploaded_resume = st.file_uploader("Upload your resume", type=['pdf'])
@@ -14,3 +38,10 @@ if uploaded_resume is not None:
     for page in doc:
         text += page.get_text()
     st.text_area("Extracted Text", text, height=300)
+    st.session_state.pdf_body = text
+    st.session_state["text_extracted"] = True
+
+if st.session_state["text_extracted"]:
+    if st.button("Extract job duties"):
+        job_duties = extract_job_duties(st.session_state.pdf_body)
+        st.write(job_duties)
